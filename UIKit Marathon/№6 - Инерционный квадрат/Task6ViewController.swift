a5818eb97333d7c3b5a9f02c7ae00b158ace979c
Tyshkan4ik <001vat@gmail.com>
2023-04-24 15:12:57 +0300

    did task number 6 - Inertial square

--- UIKit Marathon/№6 - Инерционный квадрат/Task6ViewController.swift
+++ UIKit Marathon/№6 - Инерционный квадрат/Task6ViewController.swift
@@ -9,21 +9,54 @@ import UIKit
 
 class Task6ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    private enum Constants {
+        static let sideLenght: CGFloat = 100
+        static let cornerRadius: Double = 10
+        static let snapValue: CGFloat = 1
+        static let two: CGFloat = 2
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: - Properties
+    
+    private lazy var squareView: UIView = {
+        let view = UIView(
+            frame: CGRect(
+                x: view.center.x - Constants.sideLenght / Constants.two,
+                y: view.center.y - Constants.sideLenght / Constants.two,
+                width: Constants.sideLenght,
+                height: Constants.sideLenght
+            )
+        )
+        view.backgroundColor = .systemBlue
+        view.layer.cornerRadius = Constants.cornerRadius
+        return view
+    }()
+    
+    var animator: UIDynamicAnimator!
+    //пружинное поведение
+    var snap: UISnapBehavior!
+    
+    //MARK: - Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupElements()
+        animator = UIDynamicAnimator(referenceView: view)
     }
-    */
-
+    
+    private func setupElements() {
+        view.addSubview(squareView)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // удаляем SnapBehavior каждый раз после отработки анимации, чтобы корректно работал при каждом клике
+        if snap != nil {
+            animator.removeBehavior(snap)
+        }
+        let touch = touches.first!
+        snap = UISnapBehavior(item: squareView, snapTo: touch.location(in: view))
+        snap.damping = Constants.snapValue
+        animator.addBehavior(snap)
+    }
 }
